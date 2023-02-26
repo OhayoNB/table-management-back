a620900b61cfbb7433c505a4ec286d10fe174955
Shahar Cohen <scohen@fireblocks.com>
2023-02-25 17:45:32 -0800

    update CORS

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === 'production') {
       'http://127.0.0.1:3000',
       'http://localhost:3000',
       'https://theproductloundge.com',
+      'https://myseat.theproductlounge.com',
     ],
     credentials: true,
   }
